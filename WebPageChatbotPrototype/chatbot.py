22f7de8dc92c07658e09c0ad49602522d56b6ca2
nikola-stevanovic14 <nikolastevanovic815@gmail.com>
2021-05-30 21:33:16 +0200

    before index place finding - problem solved

--- WebPageChatbotPrototype/chatbot.py
+++ WebPageChatbotPrototype/chatbot.py
@@ -94,7 +94,7 @@ while True:
         index += 3
         element = message[index]
         index = webpageString.find('id = ' + element)
-        index -= 12
+        index = webpageString[:index].rfind('<BEFORE>')
         webpageString = webpageString[:index] + res + webpageString[index:]
     elif i_class == "after":
         index = message.find('id')
